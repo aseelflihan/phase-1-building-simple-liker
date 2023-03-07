022f5b81b4bdafd35eb32b8d5b06fe4c09842d9b
aseelflihan <aseelflihan@gmail.com>
2023-03-07 11:21:36 +0300

    complated assignment

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.getElementById('modal')
+const errorMessage = document.getElementById('modal-message')
 
-// Your JavaScript code goes here!
+modal.classList.add('hidden')
 
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph')
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.innerText === EMPTY_HEART) {
+            heart.innerText = FULL_HEART
+            heart.classList.add('activated-heart')
+          } else {
+            heart.innerText = EMPTY_HEART
+            heart.classList.remove('activated-heart')
+          }
+        })
+        .catch(error => {
+          errorMessage.innerText = error
+          modal.classList.remove('hidden')
+          setTimeout(() => {
+            modal.classList.add('hidden')
+          }, 3000)
+        })
+    })
+  })
+})
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
